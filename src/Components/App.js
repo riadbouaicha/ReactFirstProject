fea77e01e941ccb550836ce6bfa83ec171e0ff41
riadbouaicha <riadbouaicha@gmail.com>
2022-05-31 15:40:30 +0200

    First commit

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+//import './App.css';
+import Banner from './Banner'
+import ShoppingList from './ShoppingList'
+
+function App() {
+	return (
+		<div>
+			<Banner />
+			
+			<ShoppingList />
+		</div>
+	)
+}
+
+export default App
